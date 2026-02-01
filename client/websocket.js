8387ecb6c133a4fca96e1493540e0dde0314cd06
Bonthu Poojitha <bonthupoojitha123@gmail.com>
2026-02-02 01:32:51 +0530

    Updated websocket

--- client/websocket.js
+++ client/websocket.js
@@ -1,7 +1,17 @@
-export const socket = io("http://localhost:3000");
+export const socket = io(
+  "https://collaborative-canvas-bz4g.onrender.com",
+  {
+    transports: ["websocket"],
+    secure: true,
+  }
+);
 
 socket.on("connect", () => {
   console.log("Connected to WebSocket server:", socket.id);
+});
+
+socket.on("connect_error", (err) => {
+  console.error("Socket connection error:", err.message);
 });
 
 socket.on("disconnect", () => {
